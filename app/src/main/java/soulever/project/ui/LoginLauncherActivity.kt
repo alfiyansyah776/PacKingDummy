b64c5d97e1600dc47a8c03eb002beb739f1a0808
alfiyansyah776 <muhammad.alfiyansyah@ui.ac.id>
2021-05-14 14:50:12 +0700

    commit lagi

--- app/src/main/java/soulever/project/ui/LoginLauncherActivity.kt
+++ app/src/main/java/soulever/project/ui/LoginLauncherActivity.kt
@@ -1,12 +1,27 @@
 package soulever.project.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import soulever.project.R
+import soulever.project.databinding.ActivityLoginLauncherBinding
 
-class LoginLauncherActivity : AppCompatActivity() {
+class LoginLauncherActivity : AppCompatActivity(){
+    private lateinit var LoginLauncherBinding : ActivityLoginLauncherBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_launcher)
+        LoginLauncherBinding = ActivityLoginLauncherBinding.inflate(layoutInflater)
+        setContentView(LoginLauncherBinding.root)
+
+        LoginLauncherBinding.login.setOnClickListener {
+            val intent = Intent(this@LoginLauncherActivity, LoginActivity::class.java )
+            startActivity(intent)
+        }
+
+        LoginLauncherBinding.register.setOnClickListener {
+            val intent = Intent(this@LoginLauncherActivity, RegisterActivity::class.java )
+            startActivity(intent)
+        }
     }
 }
