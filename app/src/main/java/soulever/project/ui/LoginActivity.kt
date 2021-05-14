4a8b057f31d4a86d480731495903d0209057ceb9
alfiyansyah776 <muhammad.alfiyansyah@ui.ac.id>
2021-05-14 14:27:40 +0700

    commit terus

---
+++ app/src/main/java/soulever/project/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package soulever.project.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import soulever.project.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/soulever/project/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package soulever.project.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import soulever.project.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
