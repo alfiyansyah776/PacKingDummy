221116e8afb05a2112c54cf086187f0be75c040a
alfiyansyah776 <muhammad.alfiyansyah@ui.ac.id>
2021-05-18 15:40:27 +0700

    progress selasa, 18 Mei

--- app/src/main/java/soulever/project/ui/LoginActivity.kt
+++ app/src/main/java/soulever/project/ui/LoginActivity.kt
@@ -3,24 +3,56 @@ package soulever.project.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import soulever.project.R
 import soulever.project.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var loginActivityBinding : ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         loginActivityBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(loginActivityBinding.root)
 
-        loginActivityBinding.LoginButton.setOnClickListener {
+        auth = FirebaseAuth.getInstance()
+
+        /*loginActivityBinding.LoginButton.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
-        }
+        }*/
+
+        Login()
 
         loginActivityBinding.register.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun Login(){
+        loginActivityBinding.LoginButton.setOnClickListener {
+            if (TextUtils.isEmpty(loginActivityBinding.Email.text.toString())){
+                loginActivityBinding.Email.setError("Please enter your email")
+                return@setOnClickListener
+            } else if (TextUtils.isEmpty(loginActivityBinding.Password.text.toString())){
+                loginActivityBinding.Password.setError("Please enter your password")
+                return@setOnClickListener
+            }
+
+            auth.signInWithEmailAndPassword(loginActivityBinding.Email.text.toString(), loginActivityBinding.Password.text.toString())
+                .addOnCompleteListener {
+                    if(it.isSuccessful){
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(this@LoginActivity, "Login Failed, please try again!", Toast.LENGTH_LONG).show()
+
+                    }
+                }
         }
     }
 }
--- app/src/main/java/soulever/project/ui/RegisterActivity.kt
+++ app/src/main/java/soulever/project/ui/RegisterActivity.kt
@@ -3,19 +3,66 @@ package soulever.project.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import soulever.project.R
 import soulever.project.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var activityRegisterBinding : ActivityRegisterBinding
+    private lateinit var auth : FirebaseAuth
+    var databaseReference : DatabaseReference? = null
+    var database : FirebaseDatabase? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityRegisterBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(activityRegisterBinding.root)
 
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance()
+        databaseReference = database?.reference!!.child("Profile")
+
+        register()
+
         activityRegisterBinding.ayologin.setOnClickListener {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+
+
+    }
+
+    private fun register(){
+        activityRegisterBinding.LoginButton.setOnClickListener {
+            if (TextUtils.isEmpty(activityRegisterBinding.username.text.toString())){
+                activityRegisterBinding.username.setError("Please Enter Username")
+                return@setOnClickListener
+            } else if (TextUtils.isEmpty(activityRegisterBinding.email.text.toString())){
+                activityRegisterBinding.email.setError("Please Enter your Email")
+                return@setOnClickListener
+            } else if (TextUtils.isEmpty(activityRegisterBinding.Password.text.toString())){
+                activityRegisterBinding.Password.setError("Please Enter Your Password")
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(activityRegisterBinding.email.text.toString(), activityRegisterBinding.Password.text.toString())
+                .addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val currentUser = auth.currentUser
+                        val currentUserdb = databaseReference?.child((currentUser?.uid!!))
+                        currentUserdb?.child("Username")?.setValue(activityRegisterBinding.username.text.toString())
+
+                        Toast.makeText(this@RegisterActivity, "Registration Success", Toast.LENGTH_LONG).show()
+                        finish()
+                    }else {
+                        Toast.makeText(this@RegisterActivity, "Registration Failed, please try again!", Toast.LENGTH_LONG).show()
+                    }
+                }
+
         }
     }
 }
