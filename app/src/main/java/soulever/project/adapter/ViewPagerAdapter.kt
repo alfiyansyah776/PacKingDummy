1162eeed375ee9206984a962d24ac64455171790
adehndr <adehendra@outlook.com>
2021-05-31 12:15:12 +0700

    Fix home, masang viewpager

---
+++ app/src/main/java/soulever/project/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,26 @@
+package soulever.project.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import soulever.project.ui.fragment.HomeFragment
+import soulever.project.ui.fragment.NotifikasiFragment
+import soulever.project.ui.fragment.PesananFragment
+import soulever.project.ui.fragment.ProfilFragment
+
+class ViewPagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity)  {
+
+    override fun getItemCount(): Int = 4
+
+    override fun createFragment(position: Int): Fragment {
+
+        return when (position) {
+            0 -> HomeFragment()
+            1 -> PesananFragment()
+            2 -> NotifikasiFragment()
+            else -> ProfilFragment()
+        }
+    }
+}
